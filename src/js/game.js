56892761737dc09e545f3c286aafeaeec568f888
Xytovl <xytovl-git@laposte.net>
2013-09-08 14:58:37 +0200

    Some eye candy

--- src/js/game.js
+++ src/js/game.js
@@ -53,8 +53,6 @@ Squarez.prototype =
 
 	updateBoard: function(transition)
 	{
-		this.board.applyTransition(transition);
-
 		var old = this.root.getElementsByClassName("transient");
 		for (var i = 0 ; i < old.length ; i++)
 		{
@@ -83,6 +81,8 @@ Squarez.prototype =
 			}
 		};
 		
+		var removed = new Array();
+		
 		for (var i = 0 ; i < transitionSize ; i++)
 		{
 			var cell = transition.get(i);
@@ -91,12 +91,9 @@ Squarez.prototype =
 				var e = this.getCell(cell.fromx, cell.fromy);
 				e.classList.add("removed");
 				e.classList.add("transient");//FIXME: Firefox does not support add with multiple arguments
+				removed.push({x:cell.fromx, y:cell.fromy, symbol:this.board.get(cell.fromx, cell.fromy)});
 			}
-		}
-		for (var i = 0 ; i < transitionSize ; i++)
-		{
-			var cell = transition.get(i);
-			if (!cell.removed)
+			else
 			{
 				if (cell.isNew())
 				{
@@ -110,7 +107,55 @@ Squarez.prototype =
 				}
 			}
 		}
-		this.board.print();
+		if (removed.length == 4)
+		{
+			var center = {x:0, y:0};
+			for (var i = 0 ; i < 4 ; i++)
+			{
+				center.x += removed[i].x;
+				center.y += removed[i].y;
+			}
+			center.x /=4;
+			center.y /=4;
+			var squareSize = (removed[0].x - removed[1].x)*(removed[0].x - removed[1].x) + (removed[0].y - removed[1].y)*(removed[0].y - removed[1].y);
+			var side = 1;
+			var otherSize = (removed[0].x - removed[2].x)*(removed[0].x - removed[2].x) + (removed[0].y - removed[2].y)*(removed[0].y - removed[2].y);
+			if (otherSize < squareSize)
+			{
+				squareSize = otherSize;
+				side = 2;
+			}
+			squareSize = Math.sqrt(squareSize);
+			Module.print("square size: "+squareSize);
+			var n = document.createElement("div");
+			n.style.top = ""+(0.5+center.y)+"em";
+			n.style.left = ""+(0.5+center.x)+"em";
+			n.classList.add("transient");
+			n.classList.add("transition-square-container");
+			n.style.position = "absolute";
+			var n1 = document.createElement("div");
+			n.appendChild(n1);
+			n1.style.marginLeft = "-50%";
+			n1.style.marginTop = "-50%";
+			n1.style.width = ""+squareSize + "em";
+			n1.style.height = ""+squareSize + "em";
+			n1.classList.add("transition-square");
+			n1.classList.add("symbol"+removed[0].symbol);
+			var rotation = Math.atan2(removed[0].y - removed[side].y, removed[0].x - removed[side].x);
+			if (n.style.transform !== undefined)
+			{
+				n.style.transform = "rotate("+rotation+"rad)";
+				n.style.transformOrigin = "0 0"
+			}
+			if (n.style.webkitTransform !== undefined)
+			{
+				n.style.webkitTransform = "rotate("+rotation+"rad)";
+				n.style.webkitTransformOrigin = "0 0"
+			}
+			setTimeout(function() {n.classList.add("transition-removed");}, 100)
+			this.root.appendChild(n);
+		}
+		this.board.applyTransition(transition);
 	},
 
 	select: function(el)
