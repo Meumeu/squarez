1e1796849c5bdca03458c887614d42c6538e9f00
Xytovl <xytovl-git@laposte.net>
2013-09-11 23:06:42 +0200

    Make server able to create the socket (--port argument)

--- src/server/server.cpp
+++ src/server/server.cpp
@@ -28,14 +28,29 @@
 #include <systemd/sd-daemon.h>
 #endif
 
+#include <sys/socket.h>
+#include <netinet/in.h>
+
 // FastCGI
 #include <fastcgi++/manager.hpp>
 
+#include <boost/program_options.hpp>
+
 #include "server/requesthandler.h"
 
-int main()
+int main(int argc, char ** argv)
 {
-	int socket = -1;
+	// Declare program options
+	boost::program_options::options_description options;
+	options.add_options()
+		("port", boost::program_options::value<int>(), "Listen to the given port");
+
+	// Parse command line parameters
+	boost::program_options::variables_map parameters;
+	boost::program_options::store(boost::program_options::parse_command_line(argc, argv, options), parameters);
+	boost::program_options::notify(parameters);
+
+	int socket_fd = -1;
 
 #ifndef DISABLE_SYSTEMD
 	// Check systemd socket activation
@@ -47,18 +62,42 @@ int main()
 	}
 	if (systemd_fds == 1)
 	{
-		socket = SD_LISTEN_FDS_START;
+		socket_fd = SD_LISTEN_FDS_START;
 	}
 #endif
 
 	// If we don't have an open socket, create it
-	if (socket < 0)
+	if (socket_fd < 0 and parameters.count("port"))
 	{
-		//FIXME: create the socket, really
+		socket_fd = socket(PF_INET, SOCK_STREAM, 0);
+		if (socket_fd == -1)
+		{
+			std::cerr << "Failed to create socket." << std::endl;
+			return 1;
+		}
+
+		struct sockaddr_in addr;
+		bzero(&addr, sizeof(addr));
+		addr.sin_family = AF_INET;
+		addr.sin_port = htons(parameters["port"].as<int>());
+		addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+
+		if (bind(socket_fd, (struct sockaddr *) &addr, sizeof(addr)) < 0
+			or listen(socket_fd, 128))
+		{
+			std::cerr << "Failed to create socket." << std::endl;
+			return 1;
+		}
+	}
+
+	if (socket_fd < 0)
+	{
+		std::cerr << "No socket available." << std::endl;
+		return 1;
 	}
 
 	// Start listening on the provided socket
-	Fastcgipp::Manager<squarez::RequestHandler> manager(socket);
+	Fastcgipp::Manager<squarez::RequestHandler> manager(socket_fd);
 	try{
 		manager.handler();
 		return 0;
