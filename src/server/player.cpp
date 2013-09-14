0d538fef1dd100e7b1487d17968f289ea17efaaf
Xytovl <xytovl-git@laposte.net>
2013-09-14 22:10:04 +0200

    oops

---
+++ src/server/player.cpp
@@ -0,0 +1,48 @@
+/*
+ * Squarez puzzle game server binary
+ * Copyright (C) 2013  Patrick Nicolas <patricknicolas@laposte.net>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+#include "player.h"
+
+namespace squarez
+{
+Player::Player(std::string name): _score(0), _name(name), _roundScore(0), _previousScore(1)
+{
+
+}
+
+void Player::endGame()
+{
+	_previousScore = _score + _roundScore;
+	_score = 0;
+	_roundScore = 0;
+}
+
+void Player::endRound()
+{
+	_score += _roundScore;
+	_roundScore = 0;
+}
+
+void Player::setRoundScore(unsigned int roundScore)
+{
+	if (roundScore > _roundScore)
+		_roundScore = roundScore;
+}
+
+}
---
+++ src/server/player.h
@@ -0,0 +1,56 @@
+/*
+ * Squarez puzzle game server binary
+ * Copyright (C) 2013  Patrick Nicolas <patricknicolas@laposte.net>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+#ifndef SQUAREZ_PLAYER_H
+#define SQUAREZ_PLAYER_H
+
+#include <string>
+
+namespace squarez {
+
+class Player
+{
+public:
+	Player(std::string name);
+
+	std::string const& getName() const { return _name;}
+
+	unsigned int getScore() const { return _score + _roundScore;}
+
+	unsigned int getPreviousScore() const { return _previousScore;}
+
+	void endGame();
+	void endRound();
+
+	void setRoundScore(unsigned int roundScore);
+	unsigned int getRoundScore() const { return _roundScore;}
+private:
+	std::string const _name;
+	// Score during the current game
+	unsigned int _score;
+
+	// Total score of previous game
+	unsigned int _previousScore;
+
+	// Score of the best transition of the current round
+	unsigned int _roundScore;
+};
+}
+
+#endif // SQUAREZ_PLAYER_H
