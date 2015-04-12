6eb0c16c2f1685c843ae6b7b2cc485404320e0de
Guillaume Meunier <guillaume.meunier@centraliens.net>
2015-04-12 15:02:45 +0200

    French translation

---
+++ translations/fr.ts
@@ -0,0 +1,93 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr">
+<context>
+    <name>FirstPage</name>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="64"/>
+        <source>Plop</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="83"/>
+        <source>How to play</source>
+        <translation>Comment jouer</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="107"/>
+        <source>New game</source>
+        <translation>Nouvelle partie</translation>
+    </message>
+</context>
+<context>
+    <name>GameArea</name>
+    <message>
+        <location filename="../../data/qml/GameArea.qml" line="143"/>
+        <source>Game over</source>
+        <translation>Game over</translation>
+    </message>
+</context>
+<context>
+    <name>NameInput</name>
+    <message>
+        <location filename="../qml/pages/NameInput.qml" line="24"/>
+        <source>Enter your name</source>
+        <translation>Entrez votre nom</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/NameInput.qml" line="32"/>
+        <source>Your name</source>
+        <translation>Votre nom</translation>
+    </message>
+</context>
+<context>
+    <name>SinglePlayerPage</name>
+    <message>
+        <location filename="../qml/pages/SinglePlayerPage.qml" line="26"/>
+        <source>Score: %1</source>
+        <translation>Score : %1</translation>
+    </message>
+</context>
+<context>
+    <name>TutorialPage</name>
+    <message>
+        <location filename="../qml/pages/TutorialPage.qml" line="11"/>
+        <source>Squarez rules
+(click to continue)</source>
+        <translation>Règles de squarez
+(cliquer pour continuer)</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/TutorialPage.qml" line="14"/>
+        <source>Spot similar elements at the edges of a square shape</source>
+        <translation>Trouvez les éléments identiques aux sommets d&apos;un carré</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/TutorialPage.qml" line="17"/>
+        <source>Bigger squares give more points</source>
+        <translation>Les carrés plus grands donnent plus de points</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/TutorialPage.qml" line="20"/>
+        <source>Squares not aligned with the grid give double score</source>
+        <translation>Les carrés non alignés avec la grille doublent le score</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/TutorialPage.qml" line="23"/>
+        <source>Time is limited
+Each square you select refills time based on its score</source>
+        <translation>Le temps est limité
+Chaque carré donne du temps en fonction du score</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/TutorialPage.qml" line="26"/>
+        <source>Total amount of available time gets shorter as you progress</source>
+        <translation>Le temps total devient de plus en plus court</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/TutorialPage.qml" line="42"/>
+        <source>Score: %1</source>
+        <translation>Score : %1</translation>
+    </message>
+</context>
+</TS>
