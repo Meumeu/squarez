049111ff9a8624f8b20f3dba6e606969544169dd
Guillaume Meunier <guillaume.meunier@centraliens.net>
2015-04-18 09:29:34 +0200

    Update translations

--- translations/en.ts
+++ translations/en.ts
@@ -4,17 +4,37 @@
 <context>
     <name>FirstPage</name>
     <message>
-        <location filename="../qml/pages/FirstPage.qml" line="64"/>
-        <source>Plop</source>
-        <translation type="unfinished"></translation>
+        <location filename="../sailfish/qml/pages/FirstPage.qml" line="34"/>
+        <source>Last week</source>
+        <translation>Last week</translation>
     </message>
     <message>
-        <location filename="../qml/pages/FirstPage.qml" line="83"/>
+        <location filename="../sailfish/qml/pages/FirstPage.qml" line="37"/>
+        <source>Last month</source>
+        <translation>Last month</translation>
+    </message>
+    <message>
+        <location filename="../sailfish/qml/pages/FirstPage.qml" line="40"/>
+        <source>All time</source>
+        <translation>All time</translation>
+    </message>
+    <message>
+        <location filename="../sailfish/qml/pages/FirstPage.qml" line="90"/>
+        <source>High scores</source>
+        <translation>High scores</translation>
+    </message>
+    <message>
+        <location filename="../sailfish/qml/pages/FirstPage.qml" line="139"/>
+        <source>No items</source>
+        <translation>No items</translation>
+    </message>
+    <message>
+        <location filename="../sailfish/qml/pages/FirstPage.qml" line="147"/>
         <source>How to play</source>
         <translation>How to play</translation>
     </message>
     <message>
-        <location filename="../qml/pages/FirstPage.qml" line="107"/>
+        <location filename="../sailfish/qml/pages/FirstPage.qml" line="177"/>
         <source>New game</source>
         <translation>New game</translation>
     </message>
@@ -22,7 +42,7 @@
 <context>
     <name>GameArea</name>
     <message>
-        <location filename="../../data/qml/GameArea.qml" line="143"/>
+        <location filename="../data/qml/GameArea.qml" line="143"/>
         <source>Game over</source>
         <translation>Game over</translation>
     </message>
@@ -30,20 +50,38 @@
 <context>
     <name>NameInput</name>
     <message>
-        <location filename="../qml/pages/NameInput.qml" line="24"/>
+        <location filename="../sailfish/qml/pages/NameInput.qml" line="36"/>
+        <source>Start</source>
+        <translation>Start</translation>
+    </message>
+    <message>
+        <location filename="../sailfish/qml/pages/NameInput.qml" line="42"/>
+        <source>Play online</source>
+        <translation>Play online</translation>
+    </message>
+    <message>
+        <location filename="../sailfish/qml/pages/NameInput.qml" line="50"/>
         <source>Enter your name</source>
         <translation>Enter your name</translation>
     </message>
+</context>
+<context>
+    <name>SettingsPage</name>
     <message>
-        <location filename="../qml/pages/NameInput.qml" line="32"/>
-        <source>Your name</source>
-        <translation>Your name</translation>
+        <location filename="../sailfish/qml/pages/SettingsPage.qml" line="27"/>
+        <source>Settings</source>
+        <translation>Settings</translation>
     </message>
 </context>
 <context>
     <name>SinglePlayerPage</name>
     <message>
-        <location filename="../qml/pages/SinglePlayerPage.qml" line="26"/>
+        <location filename="../sailfish/qml/pages/SinglePlayerPage.qml" line="16"/>
+        <source>Network error: score will not be saved</source>
+        <translation>Network error: score will not be saved</translation>
+    </message>
+    <message>
+        <location filename="../sailfish/qml/pages/SinglePlayerPage.qml" line="36"/>
         <source>Score: %1</source>
         <translation>Score: %1</translation>
     </message>
@@ -51,41 +89,41 @@
 <context>
     <name>TutorialPage</name>
     <message>
-        <location filename="../qml/pages/TutorialPage.qml" line="11"/>
+        <location filename="../sailfish/qml/pages/TutorialPage.qml" line="11"/>
         <source>Squarez rules
 (click to continue)</source>
         <translation>Squarez rules
 (click to continue</translation>
     </message>
     <message>
-        <location filename="../qml/pages/TutorialPage.qml" line="14"/>
+        <location filename="../sailfish/qml/pages/TutorialPage.qml" line="14"/>
         <source>Spot similar elements at the edges of a square shape</source>
         <translation>Spot similar elements at the edges of a square shape</translation>
     </message>
     <message>
-        <location filename="../qml/pages/TutorialPage.qml" line="17"/>
+        <location filename="../sailfish/qml/pages/TutorialPage.qml" line="17"/>
         <source>Bigger squares give more points</source>
         <translation>Bigger squares give more points</translation>
     </message>
     <message>
-        <location filename="../qml/pages/TutorialPage.qml" line="20"/>
+        <location filename="../sailfish/qml/pages/TutorialPage.qml" line="20"/>
         <source>Squares not aligned with the grid give double score</source>
         <translation>Squares not aligned with the grid give double score</translation>
     </message>
     <message>
-        <location filename="../qml/pages/TutorialPage.qml" line="23"/>
+        <location filename="../sailfish/qml/pages/TutorialPage.qml" line="23"/>
         <source>Time is limited
 Each square you select refills time based on its score</source>
         <translation>Time is limited
 Each square you select refills time based on its score</translation>
     </message>
     <message>
-        <location filename="../qml/pages/TutorialPage.qml" line="26"/>
+        <location filename="../sailfish/qml/pages/TutorialPage.qml" line="26"/>
         <source>Total amount of available time gets shorter as you progress</source>
         <translation>Total amount of available time gets shorter as you progress</translation>
     </message>
     <message>
-        <location filename="../qml/pages/TutorialPage.qml" line="42"/>
+        <location filename="../sailfish/qml/pages/TutorialPage.qml" line="42"/>
         <source>Score: %1</source>
         <translation>Score: %1</translation>
     </message>
