22bddd87a72954945755766d36325e63027b1ea0
Guillaume Meunier <guillaume.meunier@centraliens.net>
2015-04-25 15:07:20 +0200

    Add a stack trace in database exceptions

--- shared/database/database.cpp
+++ shared/database/database.cpp
@@ -25,10 +25,21 @@
 #include <thread>
 #include <boost/lexical_cast.hpp>
 
+#include <execinfo.h>
+
 namespace squarez {
 
 database::exception::exception(const std::string& msg): _msg(msg)
 {
+	void * trace_elems[20];
+	int trace_elem_count = backtrace(trace_elems, 20);
+	char ** stack_syms = backtrace_symbols(trace_elems, trace_elem_count);
+	for(int i = 0 ; i < trace_elem_count ; ++i)
+	{
+		_msg += "\n";
+		_msg += stack_syms[i];
+	}
+	free(stack_syms);
 }
 
 const char* database::exception::what() const noexcept
